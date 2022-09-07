067c0b808bc45345b0ee0e3f0d4c9d891ac098af
석재현 <rkdrl45617@gmail.com>
2022-09-07 16:20:24 +0900

    초기 환경 셋팅

---
+++ src/main/java/com/jrd/java_real_dev/JavaRealDevApplication.java
@@ -0,0 +1,13 @@
+package com.jrd.java_real_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRealDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaRealDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jrd/java_real_dev/JavaRealDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jrd.java_real_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRealDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
